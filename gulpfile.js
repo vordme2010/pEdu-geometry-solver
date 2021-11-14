a0899736635bc3f5b217f2dfc43b5def0abe6e0d
ivanbezkrovny <vordme2010@gmail.com>
2021-11-14 16:04:31 +0200

    initial state

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+const gulp = require('gulp');
+const createGulpSass = require('gulp-sass');
+const sassEngine = require('sass');
+const sass = createGulpSass(sassEngine);
+const del = require('del');
+const concat = require('gulp-concat');
+const cleanCSS = require('gulp-clean-css');
+const uglifyJs = require('gulp-uglify');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+const imagemin = require('gulp-imagemin');
+const purgeCss = require('gulp-purgecss');
+
+
+const compile = function() {
+    gulp.src("src/**/*.html")
+        .pipe(gulp.dest("./dist/"))
+    gulp.src('./src/**/*.js')
+        .pipe(concat('scripts.js'))
+        .pipe(uglifyJs())
+        .pipe(gulp.dest('./dist/scripts/'))
+    return gulp.src('./src/styles/styles.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            cascade: false
+        }))
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(gulp.dest('./dist/styles/'))
+        .pipe(browserSync.stream());
+}
+gulp.task('build', function() {
+    del.sync('dist');
+    gulp.src('./src/img/**')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/img/'))
+    gulp.src('./src/styles/styles.scss')
+        .pipe(purgeCss({
+            content: ['src/**/*.html']
+        }))
+    compile()
+})
+gulp.task('dev', function() {
+    browserSync.init({
+        server: "./dist"
+    });
+    compile()
+    gulp.watch(['./src/**/*.scss', './src/**/*.js', "./src/*.html", './src/img/*'], compile);
+    gulp.watch("./src/*.html").on('change', browserSync.reload);
+});
---
+++ readme.md
@@ -0,0 +1,3 @@
+Who we are:
+            program that will help you to improve your knowledge of geometry, 
+            solve and prove nonlinear geometric problems.
